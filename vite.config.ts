4f2d248f5716db380c4cae519cdaaab8b6df3f8f
eamesh <easeava@gmail.com>
2022-06-05 22:34:30 +0800

    feat: support jsx/tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, PluginOption } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { resolve } from 'path';
+
+const plugins: PluginOption[] = [
+  vue(),
+  vueJsx()
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins,
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+});
